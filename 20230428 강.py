219bf83adcaf3c549ee093a3905ea1b4e9445b5c
Kim Yoona <127467055+yasaeng@users.noreply.github.com>
2023-05-19 01:02:47 +0900

    hw7

---
+++ 20230428 강.py
@@ -0,0 +1,17 @@
+shopping_bag = {}
+while True:
+    print('\n[구입]')
+    item = input('상품명? ')
+    if item == '':
+        break
+    else:
+        nums = int(input('수량은?'))
+        shopping_bag[f'{item}'] = nums
+        print(f'장바구니에 {item} {nums}개가 담겼습니다.')
+
+print(f'>>>장바구니 보기: {shopping_bag}')
+
+print('\n[검색]')
+search = input('장바구니에서 확인하고자하는 상품은? ')
+ans = shopping_bag.get(search)
+print(f'{search}은(는) {ans}개 담겨있습니다.')
